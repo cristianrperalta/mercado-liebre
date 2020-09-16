372591266b74baac7bc1d413c8427cef572b8c20
cristianrperalta <cristian.r.peralta@gmail.com>
2020-09-15 21:37:50 -0300

    M04C02-Ejercitación - Solución

---
+++ app.js
@@ -0,0 +1,24 @@
+// requerimos express
+const express = require('express')
+// instanciamos express
+const app = express()
+// modulo nativo de node para trabajar con rutas de archivos
+const path = require('path')
+
+// resuelve ruta home
+app.get('/', function(req, res){
+    let rutaFile = path.resolve('./vistas/index.html')
+    res.sendFile(rutaFile)
+})
+
+// resuelve ruta para archivos. Ejemplo: estilos, imagenes, etc.
+app.get('*', function(req, res){
+
+    if(req.url.includes('.')){
+        let file = path.resolve('public' + req.url)
+        return res.sendFile(file)
+    }
+    res.sendFile('Not found')
+})
+
+app.listen(3000)
